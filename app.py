734f7293f2b4a303610973588a70dce4bb575c09
vangmayee03 <vangmayee44@gmail.com>
2025-07-19 12:26:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import numpy as np
 import joblib
+import os
 
 app = Flask(__name__)
 
@@ -39,5 +40,8 @@ def predict():
 
     return render_template('result.html', risk=risk_percent, tips=tips)
 
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
